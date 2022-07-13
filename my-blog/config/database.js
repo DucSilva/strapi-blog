77d7716ccbbf432635860ca6551deb22b37e0c73
darkness <ducnm@alphatrue.com>
2022-07-13 15:52:52 +0700

    deploy database

--- my-blog/config/database.js
+++ my-blog/config/database.js
@@ -1,11 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
+  connection:
+    env("ENV") === "develop"
+      ? {
+          client: "sqlite",
+          connection: {
+            filename: path.join(
+              __dirname,
+              "..",
+              env("DATABASE_FILENAME", ".tmp/data.db")
+            ),
+          },
+          useNullAsDefault: true,
+        }
+      : {
+          client: "postgres",
+          connection: env("DATABASE_URL"),
+          useNullAsDefault: true,
+        },
 });
